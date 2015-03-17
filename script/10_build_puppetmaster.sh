2b54ef2c903e38f4cb89d07b1681fb6b11add846
Paul Morgan <jumanjiman@gmail.com>
2015-03-16 22:08:19 -0400

    (wip) puppetmaster: rebase on alpine
    to-do in this commit:
    
    * switch from apache to nginx (and unicorn?)
    
    refs:
    
    * http://www.frederickding.com/posts/2015/01/puppet-master-on-centos-7-with-nginx-and-unicorn-062270/
    * https://github.com/blog/517-unicorn
    * https://docs.puppetlabs.com/guides/install_puppet/post_install.html
    
    stretch: manage cache similar to
    http://puppet-a-day.com/blog/2014/05/20/puppet-caching/

--- script/10_build_puppetmaster.sh
+++ script/10_build_puppetmaster.sh
@@ -9,8 +9,6 @@ say Create puppetmaster image.
 base_tag="jumanjiman/puppetmaster"
 hash_tag="${base_tag}:${short_hash}"
 late_tag="${base_tag}:latest"
-pushd puppetmaster
-cp -r ../ssl .
-smitty docker build --rm -t $hash_tag . 2>&1 | tee /tmp/build-puppetmaster.out
+cp -r ssl puppetmaster/
+smitty docker build --rm -t $hash_tag puppetmaster/ 2>&1 | tee /tmp/build-puppetmaster.out
 smitty docker tag -f $hash_tag $late_tag
-popd
